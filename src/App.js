681cf89ba47f8d15a1c0da7304d2da9054ad35ba
Hieu Do <hieudngch190863@fpt.edu.vn>
2023-02-23 00:32:09 +0700

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
 
 function App() {
-  // return (
-  //   <div className='login-container'>
-  //     <Button>Hi</Button>
-  //   </div>
-  // );
+
 }
 
 export default App;
