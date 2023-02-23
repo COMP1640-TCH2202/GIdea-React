d386ee8534316065c25cb0bb70dc14456b2fd396
Hieu Do <hieudngch190863@fpt.edu.vn>
2023-02-23 13:27:17 +0700

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This will be GreenIdea Project App
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
