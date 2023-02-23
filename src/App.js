a9b55b9a441f22b0b433e9f89763966f057df4c5
Hieu Do <hieudngch190863@fpt.edu.vn>
2023-02-23 13:44:17 +0700

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='' className="App-logo" alt="logo" />
         <p>
           This will be GreenIdea Project App
         </p>
