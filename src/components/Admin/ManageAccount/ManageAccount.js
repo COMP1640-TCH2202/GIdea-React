37b5bff4c1b411ed7633dc89066a39b9a5e24482
Hieu Do <hieudngch190863@fpt.edu.vn>
2023-03-04 14:41:38 +0700

    AB#93 - Develop View all Accounts

--- src/components/Admin/ManageAccount/ManageAccount.js
+++ src/components/Admin/ManageAccount/ManageAccount.js
@@ -1,65 +1,85 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import styles from '../ManageAccount/ManageAccount.module.scss'
 import Navbar from '../../Navbar/Navbar';
-import { Routes,Route,Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import CreateAccount from '../CreateAccount/CreateAccount';
+import { Link } from 'react-router-dom';
+import { BsPencil, BsTrashFill } from "react-icons/bs";
+import { BiDetail } from "react-icons/bi";
+import axios from 'axios';
+
 const ManageAccount = () => {
+    const [accounts, setAccounts] = useState([]);
+    useEffect(() => {
+        viewAllAccounts();
+    }, [])
+
+    const viewAllAccounts =  () => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {
+                setAccounts(res.data)
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+   const handleDelete = (id, e) => {
+    console.log("delete clciked")
+            axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then((res) => {
+                setAccounts(accounts.filter(account => account.id !== id));
+                console.log(id)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+    
     return (
-        <div>
+        <div className={styles.list_account}>
+            <Navbar />
             <h1 style={{ textAlign: 'center', marginTop: "2em" }}>Account Management</h1>
             <div className={styles.add_btn}>
                 <Link to='/create-account'>Create new account</Link>
             </div>
-            <div><i class="fa fa-pencil" aria-hidden="true"></i></div>
             <table>
                 <thead>
                     <tr>
-                        <th>Account Name</th>
-                        <th>Role</th>
-                        <th>Department</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Username</th>
                         <th>Status</th>
-                        <th>Actions</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td data-label = "account-name">Hieu Do</td>
-                        <td data-label = "role">Staff</td>
-                        <td data-label = "department">Computing</td>
-                        <td data-label = "status">Online</td>
-                        <td>
-                            <button>
-                                {/* <i class="fa fa-pencil" aria-hidden="true"></i> */}
-                                </button>
-                            <button><i class="fa fa-trash" aria-hidden="true"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td data-label = "account-name">Hieu Do</td>
-                        <td data-label = "role">Staff</td>
-                        <td data-label = "department">Computing</td>
-                        <td data-label = "status">Online</td>
-                        <td>
-                            
-                        </td>
-                    </tr>
-                    <tr>
-                        <td data-label = "account-name">Hieu Do</td>
-                        <td data-label = "role">Staff</td>
-                        <td data-label = "department">Computing</td>
-                        <td data-label = "status">Online</td>
-                        <td>
-                            
-                        </td>
-                    </tr>                
+                    {accounts.map((account, index) => {
+                        return (
+                            <tr key={index}>
+                                <td data-label="account-name">{account.name}</td>
+                                <td data-label="role">{account.email}</td>
+                                <td data-label="department">{account.username}</td>
+                                <td data-label="status">Online</td>
+                                <td>
+                                    <button className={styles.btn_view}>
+                                        <BiDetail/>
+                                    </button>
+                                    <Link to = {`detail/${account.id}`}>
+                                    <button className={styles.btn_edit}>
+                                        <BsPencil />
+                                    </button>
+                                    </Link>
+                                    <button className={styles.btn_delete} onClick = {() => handleDelete(account.id)}>
+                                        <BsTrashFill />
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
-            <Routes>
-                <Route path='/create-account' element={<CreateAccount />} />
-            </Routes>
         </div>
-        
+
     );
 }
 
