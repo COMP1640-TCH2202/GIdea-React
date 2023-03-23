57c656a574635d13946eb32c65a02f10c99c3786
Hieu Do <hieudngch190863@fpt.edu.vn>
2023-03-24 00:06:46 +0700

    add and update account

--- src/components/Admin/AccountDetail/AccountDetail.js
+++ src/components/Admin/AccountDetail/AccountDetail.js
@@ -1,50 +1,122 @@
 import { React, useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
-import axios from 'axios';
+import { FcBusinessman } from "react-icons/fc";
+import * as departmentService from '../../../services/DepartmentsService'
+import * as accountService from '../../../services/AccountsService'
 import '../AccountDetail/AccountDetail.module.scss';
 const AccountDetail = () => {
+    const navigate = useNavigate()
+
     const [account, setAccount] = useState();
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [role, setRole] = useState('')
+    const [department, setDepartment] = useState('')
+    const [departments, setDepartments] = useState('')
 
     useEffect(() => {
         getAccount();
+        getDepartments();
+        
     }, []);
 
     const getAccount = () => {
-        axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+        accountService.getUserById(id)
             .then((res) => {
                 setAccount(res.data);
             });
     }
+    const getDepartments = () => {
+        departmentService.getAllDepartments()
+            .then((res) => {
+                setDepartments(res.data)
+                console.log(res.data)
+            })
+    }
     const { id } = useParams()
+    const [updateForm, setUpdateForm] = useState(false)
+    const toggle = () => {
+        setUpdateForm(() => !updateForm)
+    }
+
+    const handleUpdate = async (data) => {
+        const request = {
+            firstName: data.first_name,
+            lastName: data.last_name,
+            email: data.email,
+            role: data.role,
+            department: data.department
+        }
+        accountService.updateUser(id, request)
+            .then((res) => {
+                
+            })
+        
+    }
     return (
-        <div style={{width: '70%'}} className='mx-auto'>
-            {account && (
+        <div style={{ width: '70%' }} className='mx-auto'>
+            <h1 className='text-center'>Account Detail</h1>
+            <FcBusinessman fontSize='200px' />
+            {account ? (updateForm ?
                 <>
-                    <h1 className='text-center'>Account Detail</h1>
-                    <Form.Group className="mb-3 d-flex">
-                        <Form.Label>Name:</Form.Label>
-                        <Form.Control type="text" value={account.name} readOnly ='false'/>
+                    <Form.Group className="mb-3">
+                        <Form.Label>First Name:</Form.Label>
+                        <Form.Control type="text" value={firstName} onChange = {(e) => setFirstName(e.target.value)}/>
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Last Name:</Form.Label>
+                        <Form.Control type="email" value={lastName} onChange = {(e) => setLastName(e.target.value)}/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" >
                         <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email" value={account.email} readOnly ='false'/>
+                        <Form.Control type="text" value={email} onChange = {(e) => setEmail(e.target.value)}/>
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
-                        <Form.Label>Address:</Form.Label>
-                        <Form.Control type="text" value={account.address.street} readOnly ='false'/>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Role:</Form.Label>
+                        <Form.Select value={role} onChange = {(e)=> setRole(e.target.value)}>
+                           <option>admin</option>
+                           <option>coordinator</option>
+                           <option>manager</option>
+                           <option>staff</option>
+                        </Form.Select>
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
-                        <Form.Label>Username:</Form.Label>
-                        <Form.Control type="text" value={account.username} readOnly = 'false'/>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Department:</Form.Label>
+                        <Form.Select value={department} onChange = {(e)=> setDepartment(e.target.value)}>
+                           {departments.map(department => {
+                            return (
+                                <option>{department.name}</option>
+                            )
+                           })}
+                        </Form.Select>
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
-                        <Form.Label>Company:</Form.Label>
-                        <Form.Control type="text" value={account.company.name} />
-                    </Form.Group>
-                    <Button variant='success'>Update</Button>
-                </>
-            )}
+                    <Button variant='success' onClick={() => handleUpdate()}>Save</Button>
+                </> : <>
+                    <p className='mt-3'>
+                        First Name: {account.first_name}
+                    </p>
+                    <p className='mt-3'>
+                        Last Name: {account.last_name}
+                    </p>
+                    <p className='mt-3'>
+                        Email: {account.email}
+                    </p>
+                    <p className='mt-3'>
+                        Role: {account.role}
+                    </p>
+                    <p className='mt-3'>
+                        Department: {account.department != null ? account.department : 'None'}
+                    </p>
+                    <Button variant='success' onClick={() => toggle()}>Update</Button>
+                </>)
+                : null
+            }
         </div>
     );
 }
--- src/components/Admin/CreateAccount/CreateAccount.js
+++ src/components/Admin/CreateAccount/CreateAccount.js
@@ -1,89 +1,199 @@
-import axios from 'axios';
+
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import clsx from 'clsx'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from "../CreateAccount/CreateAccount.module.scss"
 import { useNavigate } from 'react-router-dom';
-
+import * as accountService from '../../../services/AccountsService'
 
 
 
 const CreateAccount = () => {
-
+    const [error, setError] = useState(null)
     const [inputData, setInputData] = useState({
-        name: '',
+        id: null,
+        first_name: '',
+        last_name: '',
         email: '',
-        username: ''
+        password: 'password',
+        role: '',
+        department: null,
+        gender: '',
+        dob: ''
     });
+    function isValidEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
     const navigate = useNavigate();
 
+    const handleEmailChange = (e) => {
+        if (!isValidEmail(e.target.value)) {
+            setError('Email is invalid')
+        } else {
+            setError(null)
+        }
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('https://jsonplaceholder.typicode.com/users', inputData)
-        .then((res) => {
-            alert("Data Posted Successfully")
-            navigate('/manage-account')
-        })
+        accountService.createUser(inputData)
+            .then((res) => {
+                alert("Data Posted Successfully")
+                navigate('/manage-account')
+                console.log(inputData)
+            })
     }
 
     return (
-
         <div className={styles.register_container}>
             <div className={styles.form_container}>
-                <Form onSubmit={() => handleSubmit}>
-                    <h1 className={styles.register_heading}>Create account</h1>
-                    <h2 className={styles.register_subHeading}>Sign in and start managing your ideas!</h2>
-                    {/* <Form.Group className="mb-5">
-                    <Form.Select className={styles.form}>
-                        <option value="admin">Admin</option>
-                        <option value="qa">QA</option>
-                        <option value="staff">Staff</option>
-                    </Form.Select>
-                </Form.Group> */}
-                    <Form.Group className="mb-5">
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter name"
-                            className={styles.form}
-                            name='name'
-                            required
-                            onChange={e => setInputData({...inputData, name: e.target.value})}
+                <div className={styles.form_normal}>
+                    <Form onSubmit={() => handleSubmit}>
+                        <h1 className={styles.register_heading}>Create account</h1>
+                        <h2 className={styles.register_subHeading}>Sign in and start managing your ideas!</h2>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-5">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter first name"
+                                        className={styles.form}
+                                        name='first_name'
+                                        required
+                                        onChange={e => setInputData({ ...inputData, first_name: e.target.value })}
+                                    />
+                                </Form.Group>
+                                <Form.Group className="mb-5">
+                                    <Form.Control
+                                        className={styles.form}
+                                        placeholder="Enter last name"
+                                        name='last_name'
+                                        required
+                                        onChange={e => setInputData({ ...inputData, last_name: e.target.value })}
+                                    />
+                                </Form.Group>
+                                <Form.Group className="mb-5">
+                                    <Form.Control
+                                        type='date'
+                                        className={styles.form}
+                                        placeholder="Date of birth"
+                                        name='dob'
+                                        required
+                                        onChange={e => setInputData({ ...inputData, dob: e.target.value })}
+                                    />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-5">
+                                    <Form.Control
+                                        type='email'
+                                        className={styles.form}
+                                        placeholder="Enter email"
+                                        name='email'
+                                        required
+                                        onChange={() => handleEmailChange}
+                                    />
+                                </Form.Group>
+                                {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+                                {/* <Form.Group className="mb-5">
+                        <Form.Control 
+                        className={styles.form} 
+                        type="password" 
+                        placeholder="Password" 
+                        required
+                        onChange={e => setInputData({...inputData, password: e.target.value})}
                         />
-                    </Form.Group>
-                    <Form.Group className="mb-5">
-                        <Form.Control
-                            type="email"
-                            className={styles.form}
-                            placeholder="Enter email"
-                            name='email'
-                            required
-                            onChange={e => setInputData({...inputData, email: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-5">
-                        <Form.Control
-                            type="text"
-                            className={styles.form}
-                            placeholder="Enter username"
-                            name='email'
-                            required
-                            onChange={e => setInputData({...inputData, username: e.target.value})} 
-                        />
-                    </Form.Group>
-                    {/* <Form.Group className="mb-5">
-                    <Form.Control className={styles.form} type="password" placeholder="Confirm password" />
-                </Form.Group> */}
+                    </Form.Group> */}
+                                <Form.Group className="mb-5">
+                                    <Form.Select className={styles.form}>
+                                        <option value="admin">Admin</option>
+                                        <option value="coordinator">QA Coordinator</option>
+                                        <option value="staff">Staff</option>
+                                        <option value="manager">QA Manager</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="mb-5">
+                                    <Form.Control
+                                        className={styles.form}
+                                        placeholder="Department"
+                                        onChange={e => setInputData({ ...inputData, department: e.target.value })}
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Button className={styles.Button} type="submit">
+                            Register
+                        </Button>
+                    </Form>
+                </div>
 
-                    <Form.Group className={clsx(styles.check_container, 'mb-3')} controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember me" />
-                    </Form.Group>
-
-                    <Button className={styles.Button} type="submit">
-                        Register
-                    </Button>
-                </Form>
+                <div className={styles.form_small}>
+                    <Form onSubmit={() => handleSubmit}>
+                        <h1 className={styles.register_heading}>Create account</h1>
+                        <h2 className={styles.register_subHeading}>Sign in and start managing your ideas!</h2>
+                        <Form.Group className="mb-5">
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter first name"
+                                className={styles.form}
+                                name='first_name'
+                                required
+                                onChange={e => setInputData({ ...inputData, first_name: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-5">
+                            <Form.Control
+                                className={styles.form}
+                                placeholder="Enter last name"
+                                name='last_name'
+                                required
+                                onChange={e => setInputData({ ...inputData, last_name: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-5">
+                            <Form.Control
+                                type='date'
+                                className={styles.form}
+                                placeholder="Date of birth"
+                                name='dob'
+                                required
+                                onChange={e => setInputData({ ...inputData, dob: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-5">
+                            <Form.Control
+                                type='email'
+                                className={styles.form}
+                                placeholder="Enter email"
+                                name='email'
+                                required
+                                onChange={() => handleEmailChange}
+                            />
+                        </Form.Group>
+                        {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+                        <Form.Group className="mb-5">
+                            <Form.Select className={styles.form}>
+                                <option value="admin">Admin</option>
+                                <option value="coordinator">QA Coordinator</option>
+                                <option value="staff">Staff</option>
+                                <option value="manager">QA Manager</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group className="mb-5">
+                            <Form.Control
+                                className={styles.form}
+                                placeholder="Department"
+                                onChange={e => setInputData({ ...inputData, department: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Button className={styles.Button} type="submit">
+                            Register
+                        </Button>
+                    </Form>
+                </div>
             </div>
         </div>
     );
