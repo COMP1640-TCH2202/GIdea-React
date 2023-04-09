93f1217f1bcdf4eab0b110b75879e8bd9401fb6f
hongsonhvt <80675760+hongsonhvt@users.noreply.github.com>
2023-04-09 21:40:18 +0700

    Update DepartmentDetail.js

--- src/components/Department/DepartmentDetail/DepartmentDetail.js
+++ src/components/Department/DepartmentDetail/DepartmentDetail.js
@@ -1,50 +1,99 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { React, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { Form, Button } from 'react-bootstrap';
+import { FcBusinessman } from "react-icons/fc";
+import * as departmentService from '../../../services/DepartmentsService'
 import '../DepartmentDetail/DepartmentDetail.module.scss';
 import { getDetailDepartment } from '../../../services/DepartmentService';
 
 const DepartmentDetail = () => {
-    const [department, setDepartment] = useState();
+    const { register, handleSubmit } = useForm()
+
+
+    const [departments, setDepartments] = useState({})
+    const [updateForm, setUpdateForm] = useState(false)
+    const [isUpdating, setIsUpdating] = useState(false)
 
     useEffect(() => {
         getDepartment();
-    }, []);
-    console.log(department)
 
-    const { id } = useParams()
-    console.log(id)
-    const  getDepartment  =  () =>   {
-        getDetailDepartment(id)
+    }, [isUpdating]);
+
+    const getDepartment = () => {
+        departmentService.getUserById(id)
             .then((res) => {
-                setDepartment(res.data);
+                setDepartments(res.data);
             });
     }
+    const onSubmit = (data) => {
+        const request = {
+            name: data.name,
+            coordinator_name: data.coordinator_name,
+            coordinator_email: data.coordinator_email,
+            number_of_members: data.number_of_members
+        }
+        console.log(request)
+        departmentService.updateUser(id, request)
+            .then((res) => {
+                setDepartments(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        setUpdateForm(!updateForm)
+        setIsUpdating(false)
+    };
+    const { id } = useParams()
+    const toggle = () => {
+        setUpdateForm(() => !updateForm)
+        setIsUpdating(true)
+    }
+
     return (
-        <div  className='p-5 mx-auto rounded square border border-success'>
-            {department && (
-                <>
-                    <h1 className='text-center mb-5'>Department Detail</h1>
-                    <Form.Group className="mb-3 d-flex">
-                        <Form.Label className="w-25 fs-4">Name:</Form.Label>
-                        <Form.Control type="text" className="fs-5"  placeholder={department.name}  />
+        <div className='p-5 mx-auto rounded square border border-success'>
+            <h1 className='text-center mb-5'>Department Detail</h1>
+            <FcBusinessman fontSize='200px' />
+            {departments ? (updateForm ?
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Group className="mb-3">
+                        <Form.Label> Name:</Form.Label>
+                        <Form.Control type="text" placeholder='Name' {...register('name')} />
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
-                        <Form.Label className="w-25 fs-4">Coordinator name:</Form.Label>
-                        <Form.Control type="text" className="fs-5"  placeholder={department.coordinator_name}/>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Coordinator Name:</Form.Label>
+                        <Form.Control type="text" placeholder='Coordinator name' {...register('coordinator_name')} />
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
-                        <Form.Label className="w-25 fs-4">Coordinator email:</Form.Label>
-                        <Form.Control type="email" className="fs-5" placeholder={department.coordinator_email}/>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Coordinator Email:</Form.Label>
+                        <Form.Control type="email" placeholder='Coordinator email' {...register('Coordinator email')} />
+                    </Form.Group> 
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Number of name:</Form.Label>
+                        <Form.Control type="number" placeholder='Number of members' {...register('number_of_members')} />
                     </Form.Group>
-                    <Form.Group className="mb-3 d-flex" >
-                        <Form.Label className="w-25 fs-4">Number of members:</Form.Label>
-                        <Form.Control type="number" className="fs-5"  placeholder={department.number_of_members}/>
-                    </Form.Group>
-                    <Button variant='success'>Update</Button>
-                </>
-            )}
+                    <Button type='submit' >Save</Button>
+                </Form>
+                : <>
+                    <p className='mt-3'>
+                        Name: {departments.name}
+                    </p>
+                    <p className='mt-3'>
+                        Coordinator Name: {departments.coordianator_name}
+                    </p>
+                    <p className='mt-3'>
+                        Coordinator Email: {departments.coordinator_email}
+                    </p>
+                    <p className='mt-3'>
+                        Number of members: {departments.number_of_members}
+                    </p>
+                    <Button variant='success' onClick={() => toggle()}>Update</Button>
+                </>)
+                : null
+            }
         </div>
     );
 }
